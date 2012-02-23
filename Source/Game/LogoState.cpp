14260b96eaba6900c531e40de974d92834d78a7a
banhtieu <just@banhtieu.com>
2012-02-23 19:32:40 +0700

    - Decrease FPS

--- Source/Game/LogoState.cpp
+++ Source/Game/LogoState.cpp
@@ -20,8 +20,8 @@ void LogoState::Render()
 {
   Graphics2D *g = Graphics2D::GetInstance();
   g->DrawTexture(logo, 20, 200, 0, 0, 512, 161);
-  g->DrawTexture(logo, 300, 300, 123*(logoFrame/10), 161, 123, 134);
-  logoFrame = (logoFrame + 1) % 40;
+  g->DrawTexture(logo, 300, 300, 123*(logoFrame/5), 161, 123, 134);
+  logoFrame = (logoFrame + 1) % 20;
 };
 
 void LogoState::Update()
