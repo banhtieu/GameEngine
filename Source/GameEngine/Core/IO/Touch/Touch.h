8c1d4e2ec6b1a68ade46e8cc7c16ea7005cb972f
banhtieu <just@banhtieu.com>
2012-03-05 21:20:10 +0700

    + Add Touch Class.

---
+++ Source/GameEngine/Core/IO/Touch/Touch.h
@@ -0,0 +1,68 @@
+//
+//  TouchEvent.h
+//  GameEngine
+//
+//  Created by Tran Khiem on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef GameEngine_TouchEvent_h
+#define GameEngine_TouchEvent_h
+
+// Type of Touch Event
+enum TouchEventType
+{
+  TOUCH_NONE,
+  TOUCH_DOWN,
+  TOUCH_MOVE,
+  TOUCH_CANCLED,
+  TOUCH_UP
+};
+
+// Touch Data
+struct TouchData
+{
+  int x;
+  int y;
+  int type;
+};
+
+// Touch
+class Touch
+{
+public:
+  Touch(int touchId, int x, int y, int type);
+  virtual ~Touch();
+  
+  virtual bool IsInRect(int x, int y, int w, int h);
+  virtual int GetDeltaX();
+  virtual int GetDeltaY();
+  
+  // Get the touch data.
+  virtual int GetType();
+  virtual int GetX();
+  virtual int GetY();
+  
+  // is Just Touch
+  virtual bool IsJustTouch();
+  
+  // Get the Touch id
+  virtual int GetTouchId();
+  
+  // is Just Touch Up
+  virtual bool IsJustUp();
+  
+private:
+  // the current Data
+  TouchData data;
+  
+  // last Data
+  TouchData lastData;
+  
+  // touch id
+  int touchId;
+};
+
+
+
+#endif
