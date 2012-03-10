093cee41e0c2ac576d48f4f95af27757fb9251ae
banhtieu <just@banhtieu.com>
2012-03-10 20:30:08 +0700

    + Add String Manager.

---
+++ Binary/Strings.h
@@ -0,0 +1,11 @@
+#ifndef EXPORTED_STRING_
+#define EXPORTED_STRING_
+enum ExportedString 
+{
+  BEGIN,
+  OPTIONS,
+  COPYRIGHT,
+  NUMBER_OF_EXPORTED_STRINGS
+};
+// Strings exported by banh.tieu String packer
+#endif
